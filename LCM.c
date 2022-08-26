87e57c50dd87835a6614025e43b61127b5c0b37e
Ramyabalusu <96291609+Ramyabalusu@users.noreply.github.com>
2022-08-26 07:08:39 +0530

    #include<stdio.h> int main() {     int a,b,i,min,gcd,lcm;     scanf("%d%d",&a,&b);     min=(a<b)?a:b;     for(i=1;i<=min;i++)     {         if(a%i==0&&b%i==0)         {           gcd=i;         }     }     lcm=(a*b)/gcd;     printf("%d",lcm); }

---
+++ LCM.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,min,gcd,lcm;
+    scanf("%d%d",&a,&b);
+    min=(a<b)?a:b;
+    for(i=1;i<=min;i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+          gcd=i;
+        }
+    }
+    lcm=(a*b)/gcd;
+    printf("%d",lcm);
+}
