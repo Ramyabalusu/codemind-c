06cb6eec6cb028fc074b50f9fcba0866229d58e7
Ramyabalusu <96291609+Ramyabalusu@users.noreply.github.com>
2022-08-27 08:18:14 +0530

    #include<stdio.h> int main() {     int a,b,i,min,gcd;     scanf("%d%d",&a,&b);     min=(a<b)?a:b;     for(i=1;i<=min;i++)     {         if(a%i==0&&b%i==0)         {           gcd=i;         }     }     printf("%d",gcd); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,min,gcd;
+    scanf("%d%d",&a,&b);
+    min=(a<b)?a:b;
+    for(i=1;i<=min;i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+          gcd=i;
+        }
+    }
+    printf("%d",gcd);
+}
